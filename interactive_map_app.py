d3bcec20ae1e8dc8c46fab1cd59fc6675778426b
Mkp-7 <mkpatel4102@gmail.com>
2025-06-04 12:15:05 -0400

    Update interactive_map_app.py

--- interactive_map_app.py
+++ interactive_map_app.py
@@ -114,7 +114,7 @@ def main():
 
     # Create map
     m = folium.Map(
-        location=[40.0500, -74.1596],  # Latitude, Longitude
+        location=[40.1500, -74.1596],  # Latitude, Longitude
         zoom_start=8,
         tiles=None
     )
